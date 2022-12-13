302216c8c961c54ced7bc531f74efeb594d6f1fc
Axel Cawe <Cawe321Mods@gmail.com>
2022-12-13 12:30:26 +0800

    Initial commit

---
+++ Patch/VulnerableChildrenPatch.cs
@@ -0,0 +1,22 @@
+﻿using Children_Are_No_Longer_Invulnerable.Helper;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.MountAndBlade;
+
+namespace Children_Are_No_Longer_Invulnerable.Patch
+{
+    [HarmonyPatch(typeof(Agent), "EquipItemsFromSpawnEquipment", typeof(bool))]
+    public static class Agent_EquipItemFromSpawnEquipmentPatch
+    {
+        [HarmonyPostfix]
+        public static void Postfix(Agent __instance)
+        {
+            if (__instance.IsHuman && __instance.Age < 18f)
+                HelperExtension.DisableInvulnerability(__instance);
+        }
+    }
+}
---
+++ SubModule.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+
+namespace Children_Are_No_Longer_Invulnerable
+{
+    public class SubModule : MBSubModuleBase
+    {
+        public static bool PatchesApplied = false;
+        protected override void OnGameStart(Game game, IGameStarter gameStarter)
+        {
+            base.OnGameStart(game, gameStarter);
+        }
+
+        public override void OnGameInitializationFinished(Game game)
+        {
+            if (SubModule.PatchesApplied)
+                return;
+            new Harmony("ChildrenAreNoLongerInvulnerable").PatchAll();
+            SubModule.PatchesApplied = true;
+        }
+    }
+}
